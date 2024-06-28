e9d26d46add2a6408c2764163929301441b6e5fe
Haoze <haoze@grvt.io>
2024-06-28 14:06:25 +0800

    Comment out hardhat test interaction tasks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,8 +2,6 @@ import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import '@openzeppelin/hardhat-upgrades';
 
-import { IGovernance__factory } from "./typechain-types/factories/contracts/interfaces/IGovernance__factory";
-import { txConfirmation, generateSignature } from "./utils";
 const config: HardhatUserConfig = {
   paths: {
     sources: "./contracts",
@@ -20,83 +18,87 @@ const config: HardhatUserConfig = {
   }
 };
 
-task("set-transaction-filterer", "Register the transaction filterer")
-  .addParam("diamondProxyAddress", "The address of the diamond proxy")
-  .addParam("txFiltererAddress", "The address of the transaction filterer")
-  .addParam("governanceAddress", "The address of the governance contract")
-  .setAction(async (taskArgs, hre) => {
-    const { diamondProxyAddress, txFiltererAddress, governanceAddress } = taskArgs;
+// import { txConfirmation, generateSignature } from "./utils";
 
-    const [operator] = await hre.ethers.getSigners();
+// import { IGovernance__factory } from "./typechain-types/factories/contracts/interfaces/IGovernance__factory";
 
-    const hyperchainAbi = [
-      "function setTransactionFilterer(address _transactionFilterer) external"
-    ];
+// task("set-transaction-filterer", "Register the transaction filterer")
+//   .addParam("diamondProxyAddress", "The address of the diamond proxy")
+//   .addParam("txFiltererAddress", "The address of the transaction filterer")
+//   .addParam("governanceAddress", "The address of the governance contract")
+//   .setAction(async (taskArgs, hre) => {
+//     const { diamondProxyAddress, txFiltererAddress, governanceAddress } = taskArgs;
 
-    // Set transaction filterer on the hyperchain
-    const iface = new hre.ethers.Interface(hyperchainAbi);
-    const calldata = iface.encodeFunctionData(
-      "setTransactionFilterer", [txFiltererAddress]
-    );
+//     const [operator] = await hre.ethers.getSigners();
 
-    const gov = IGovernance__factory.connect(governanceAddress, operator);
-    const operation = {
-      calls: [{ target: diamondProxyAddress, value: 0, data: calldata }],
-      predecessor: hre.ethers.ZeroHash,
-      salt: hre.ethers.hexlify(hre.ethers.randomBytes(32)),
-    };
+//     const hyperchainAbi = [
+//       "function setTransactionFilterer(address _transactionFilterer) external"
+//     ];
 
-    const scheduleTx = await gov.scheduleTransparent(operation, 0);
-    await scheduleTx.wait();
-    console.log("Upgrade scheduled");
+//     // Set transaction filterer on the hyperchain
+//     const iface = new hre.ethers.Interface(hyperchainAbi);
+//     const calldata = iface.encodeFunctionData(
+//       "setTransactionFilterer", [txFiltererAddress]
+//     );
 
-    const executeTX = await gov.execute(operation, { value: 0 });
-    await executeTX.wait();
-    console.log(
-      "Upgrade with target: ",
-      diamondProxyAddress
-    );
-  });
+//     const gov = IGovernance__factory.connect(governanceAddress, operator);
+//     const operation = {
+//       calls: [{ target: diamondProxyAddress, value: 0, data: calldata }],
+//       predecessor: hre.ethers.ZeroHash,
+//       salt: hre.ethers.hexlify(hre.ethers.randomBytes(32)),
+//     };
 
-task("bridge-erc20", "Bridge ERC20 tokens")
-  .addParam("token", "The token address", "0x70a0F165d6f8054d0d0CF8dFd4DD2005f0AF6B55")
-  .addParam("amount", "The amount to bridge", "10000000000000000000000")
-  .addParam("deadline", "The deposit deadline", "2000000000")
-  .addParam("bridgeProxyAddress", "The address of the bridge proxy")
-  .setAction(async (taskArgs, hre) => {
-    const { token, amount, bridgeProxyAddress, deadline } = taskArgs;
-    const [operator] = await hre.ethers.getSigners();
+//     const scheduleTx = await gov.scheduleTransparent(operation, 0);
+//     await scheduleTx.wait();
+//     console.log("Upgrade scheduled");
 
-    const tokenAbi = [
-      "function approve(address spender, uint256 amount) external returns (bool)"
-    ];
+//     const executeTX = await gov.execute(operation, { value: 0 });
+//     await executeTX.wait();
+//     console.log(
+//       "Upgrade with target: ",
+//       diamondProxyAddress
+//     );
+//   });
 
-    const tokenContract = new hre.ethers.Contract(token, tokenAbi, operator);
+// task("bridge-erc20", "Bridge ERC20 tokens")
+//   .addParam("token", "The token address", "0x70a0F165d6f8054d0d0CF8dFd4DD2005f0AF6B55")
+//   .addParam("amount", "The amount to bridge", "10000000000000000000000")
+//   .addParam("deadline", "The deposit deadline", "2000000000")
+//   .addParam("bridgeProxyAddress", "The address of the bridge proxy")
+//   .setAction(async (taskArgs, hre) => {
+//     const { token, amount, bridgeProxyAddress, deadline } = taskArgs;
+//     const [operator] = await hre.ethers.getSigners();
 
-    await txConfirmation(tokenContract.approve(bridgeProxyAddress, amount))
-    console.log(
-      `GRVTBridgeProxy approved to spend ${amount} tokens at ${token}: `);
+//     const tokenAbi = [
+//       "function approve(address spender, uint256 amount) external returns (bool)"
+//     ];
 
-    const bridgeProxy = await hre.ethers.getContractAt("GRVTBridgeProxy", bridgeProxyAddress);
-    await (await bridgeProxy.addAllowedToken(token)).wait()
-    console.log(`Allowed token ${token} added to GRVTBridgeProxy.`);
+//     const tokenContract = new hre.ethers.Contract(token, tokenAbi, operator);
 
-    const sig = await generateSignature({
-      l1Sender: operator.address,
-      l2Receiver: operator.address,
-      l1Token: token,
-      amount: amount,
-      deadline: deadline,
-      wallet: operator
-    });
+//     await txConfirmation(tokenContract.approve(bridgeProxyAddress, amount))
+//     console.log(
+//       `GRVTBridgeProxy approved to spend ${amount} tokens at ${token}: `);
 
-    console.log(
-      "Bridge transaction: ",
-      await (await bridgeProxy.deposit(operator.address, token, amount, deadline, sig.v, sig.r, sig.s, {
-        value: hre.ethers.parseUnits("1000000000000", 18),
-        gasLimit: 2900000
-      })).wait()
-    );
-  });
+//     const bridgeProxy = await hre.ethers.getContractAt("GRVTBridgeProxy", bridgeProxyAddress);
+//     await (await bridgeProxy.addAllowedToken(token)).wait()
+//     console.log(`Allowed token ${token} added to GRVTBridgeProxy.`);
+
+//     const sig = await generateSignature({
+//       l1Sender: operator.address,
+//       l2Receiver: operator.address,
+//       l1Token: token,
+//       amount: amount,
+//       deadline: deadline,
+//       wallet: operator
+//     });
+
+//     console.log(
+//       "Bridge transaction: ",
+//       await (await bridgeProxy.deposit(operator.address, token, amount, deadline, sig.v, sig.r, sig.s, {
+//         value: hre.ethers.parseUnits("1000000000000", 18),
+//         gasLimit: 2900000
+//       })).wait()
+//     );
+//   });
 
 export default config;
